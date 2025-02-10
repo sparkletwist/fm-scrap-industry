f69a1b8f47e59b3faeb96c26efcc6dcdf324554a
Sapphira Riza <sapphiriza@gmail.com>
2025-02-09 20:32:26 -0800

    wip

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -5,15 +5,51 @@ local item_tints = require("__base__.prototypes.item-tints")
 data:extend({
   {
     type = "item",
-    name = "copper-scrap",
-    icon = "__scrap-industry__/graphics/icons/copper-scrap.png",
+    name = "iron-scrap",
+    icon = "__scrap-industry__/graphics/icons/iron-scrap.png",
     subgroup = "production-scrap",
-    order = "b[material]-a[]",
+    order = "a[material]-a[iron]",
     inventory_move_sound = item_sounds.resource_inventory_move,
     pick_sound = item_sounds.resource_inventory_pickup,
     drop_sound = item_sounds.resource_inventory_move,
     stack_size = 50,
     weight = 2*kg,
-    random_tint_color = item_tints.yellowing_coal
+    random_tint_color = item_tints.iron_rust
+  },
+  {
+    type = "item",
+    name = "copper-scrap",
+    icon = "__scrap-industry__/graphics/icons/copper-scrap.png",
+    subgroup = "production-scrap",
+    order = "a[material]-b[copper]",
+    inventory_move_sound = item_sounds.resource_inventory_move,
+    pick_sound = item_sounds.resource_inventory_pickup,
+    drop_sound = item_sounds.resource_inventory_move,
+    stack_size = 50,
+    weight = 2*kg
+  },
+  {
+    type = "item",
+    name = "steel-scrap",
+    icon = "__scrap-industry__/graphics/icons/steel-scrap.png",
+    subgroup = "production-scrap",
+    order = "a[material]-c[steel]",
+    inventory_move_sound = item_sounds.resource_inventory_move,
+    pick_sound = item_sounds.resource_inventory_pickup,
+    drop_sound = item_sounds.resource_inventory_move,
+    stack_size = 50,
+    weight = 2*kg
+  },
+  {
+    type = "item",
+    name = "circuit-scrap",
+    icon = "__scrap-industry__/graphics/icons/circuit-scrap.png",
+    subgroup = "production-scrap",
+    order = "b[product]-a[circuit]",
+    inventory_move_sound = item_sounds.resource_inventory_move,
+    pick_sound = item_sounds.resource_inventory_pickup,
+    drop_sound = item_sounds.resource_inventory_move,
+    stack_size = 50,
+    weight = 2*kg
   }
 })
--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -8,10 +8,61 @@ data:extend({
     },
     category = "smelting",
     subgroup = "production-scrap",
-    allow_productivity = false,
+    allow_productivity = true,
     auto_recycle = false,
+    allow_decomposition = false,
     energy_required = 3.2,
-    ingredients = {{type="item", name="trash", amount=1}},
+    ingredients = {{type="item", name="iron-scrap", amount=2}},
     results = {{type="item", name="iron-plate", amount=1}}
-  }
+  },
+  {
+    type = "recipe",
+    name = "copper-scrap-smelting",
+    icons = {
+      {icon="__scrap-industry__/graphics/icons/copper-scrap.png", shift={-12, -12}, scale=0.4},
+      {icon="__base__/graphics/icons/copper-plate.png"}
+    },
+    category = "smelting",
+    subgroup = "production-scrap",
+    allow_productivity = true,
+    auto_recycle = false,
+    allow_decomposition = false,
+    energy_required = 3.2,
+    ingredients = {{type="item", name="copper-scrap", amount=2}},
+    results = {{type="item", name="copper-plate", amount=1}}
+  },
+  {
+    type = "recipe",
+    name = "steel-scrap-smelting",
+    icons = {
+      {icon="__scrap-industry__/graphics/icons/steel-scrap.png", shift={-12, -12}, scale=0.4},
+      {icon="__base__/graphics/icons/steel-plate.png"}
+    },
+    category = "smelting",
+    subgroup = "production-scrap",
+    allow_productivity = true,
+    auto_recycle = false,
+    allow_decomposition = false,
+    energy_required = 3.2,
+    ingredients = {{type="item", name="steel-scrap", amount=5}},
+    results = {{type="item", name="steel-plate", amount=1}}
+  },
+  {
+    type = "recipe",
+    name = "electronic-circuit-from-scrap",
+    icons = {
+      {icon="__scrap-industry__/graphics/icons/circuit-scrap.png", shift={-12, -12}, scale=0.4},
+      {icon="__base__/graphics/icons/electronic-circuit.png"}
+    },
+    category = mods["space-age"] and "electronics" or "crafting",
+    subgroup = "production-scrap",
+    allow_productivity = true,
+    auto_recycle = false,
+    allow_decomposition = false,
+    ingredients = {
+      {type="item", name="circuit-scrap", amount=2},
+      {type="item", name="copper-cable", amount=1}
+    },
+    results = {{type="item", name="electronic-circuit", amount=1}}
+  },
 })
