3b3ce9d07e85bb5494c957081fed5da37aa4fec3
sparkletwist <sparkletwist@gmail.com>
2025-03-21 01:08:07 -0500

    Add productivity bonuses to scrap recipes

--- base-data-changes.lua
+++ base-data-changes.lua
@@ -5,11 +5,17 @@ ftech.add_unlock("steel-processing", "steel-plate-from-scrap")
 ftech.add_unlock("electronics", "electronic-circuit-from-scrap")
 ftech.add_unlock("advanced-circuit", "advanced-circuit-from-scrap")
 ftech.add_unlock("processing-unit", "processing-unit-from-scrap")
+if mods["space-age"] then
+  table.insert(data.raw.technology["steel-plate-productivity"].effects, { type = "change-recipe-productivity", recipe = "steel-plate-from-scrap", change = 0.1 })
+  table.insert(data.raw.technology["processing-unit-productivity"].effects, { type = "change-recipe-productivity", recipe = "processing-unit-from-scrap", change = 0.1 })
+end
 
 if settings.startup["scrap-industry-plastic"].value then
   ftech.add_unlock("plastics", "plastic-bar-from-bits")
   if mods["space-age"] then
     ftech.add_unlock("bioflux-processing", "bioplastic-from-bits")
+	table.insert(data.raw.technology["plastic-bar-productivity"].effects, { type = "change-recipe-productivity", recipe = "plastic-bar-from-bits", change = 0.1 })
+	table.insert(data.raw.technology["plastic-bar-productivity"].effects, { type = "change-recipe-productivity", recipe = "bioplastic-from-bits", change = 0.1 })
   end
 end
 
