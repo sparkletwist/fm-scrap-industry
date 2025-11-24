2467518415f65e988d266f2868de9f68a8b5a191
SafTheLamb <safthelamb@gmail.com>
2025-11-24 09:18:18 -0800

    Fix migration script

--- migrations/0.10.0.lua
+++ migrations/0.10.0.lua
@@ -1,5 +1,6 @@
 for _,force in pairs(game.forces) do
 	if settings.startup["scrap-industry-mech"].value then
-		force.recipes["engine-from-scrap"].enabled = force.technologies["engine"].researched
+		force.recipes["engine-unit-from-scrap"].enabled = force.technologies["engine"].researched
+		force.recipes["electric-engine-unit-from-scrap"].enabled = force.technologies["electric-engine"].researched
 	end
 end
