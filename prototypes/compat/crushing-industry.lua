91b13d72ba330694ab20fb207af257480b985572
SafTheLamb <safthelamb@gmail.com>
2025-12-21 22:08:15 -0800

    Optical fiber support

--- prototypes/compat/crushing-industry.lua
+++ prototypes/compat/crushing-industry.lua
@@ -7,4 +7,13 @@ if mods["crushing-industry"] then
 	ScrapIndustry.items["refined-concrete"] = {scrap="sand", scale=4*ScrapIndustry.CHEAP}
 	ScrapIndustry.items["landfill"] = {scrap="sand", scale=5*ScrapIndustry.CHEAP, failrate=0.01}
 	ScrapIndustry.recipes["sand"] = {ignore=true}
+
+	local mercy_mode = not settings.startup["scrap-industry-no-mercy"].value
+	if mercy_mode then
+		ScrapIndustry.recipes["optical-fiber"] = {ignore=true}
+	end
+	ScrapIndustry.items["optical-fiber"] = {scrap={"copper-scrap"}, scale=ScrapIndustry.CHEAP, failrate=0.01}
+	if settings.startup["scrap-industry-plastic"].value then
+		table.insert(ScrapIndustry.items["optical-fiber"].scrap, "plastic-bits")
+	end
 end
