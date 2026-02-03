aa8c03bf2c127143bdeff2c21ecab87e29aeaf7b
sparkletwist <sparkletwist@gmail.com>
2026-02-03 16:49:43 -0600

    Work around engine limitation

--- prototypes/item-groups.lua
+++ prototypes/item-groups.lua
@@ -3,6 +3,7 @@ data:extend({
     type = "item-subgroup",
     name = "production-scrap",
     group = "intermediate-products",
-    order = "bb"
+    --order = "bb"
+	order = "jz" -- needs to be later due to inability to set an item's preferred recipe
   }
 })
